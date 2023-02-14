14e159903162263908c4e51c72c346292f284748
okanr <okan.rn@outlook.de>
2023-02-14 20:45:04 +0100

    .

--- skincancer.py
+++ skincancer.py
@@ -3,101 +3,111 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import tensorflow as tf
 from keras.callbacks import TensorBoard
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, Activation, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from keras.models import Sequential
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
-from time import time
 
 X = pickle.load(open("X.pickle", "rb"))
 Y_cat = pickle.load(open("y.pickle", "rb"))
 
-activations = ['sigmoid', 'tanh', 'leaky relu', 'relu']
-optimizers = ['SGD', 'Adagrad', 'Adadelta', 'RMSprop', 'Adam']
+activations = ['relu']
+optimizers = ['Adam']
 
 batch_size = 16
-epochs = 50
+epochs = 250
+
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y_cat, test_size=0.25, random_state=42)
 for activation in activations:
     for optimizer in optimizers:
-            NAME = "O-{}_A-{}".format(optimizer, activation)
+        NAME = "O-{}_A-{}".format(optimizer, activation)
 
-            model = Sequential()
+        model = Sequential()
 
-            model.add(Conv2D(256, (3, 3), activation=activation, input_shape=(32, 32, 3)))
-            model.add(MaxPool2D(pool_size=(2, 2)))
-            model.add(Dropout(0.3))
+        model.add(Conv2D(256, (3, 3), activation=activation, input_shape=(32, 32, 3)))
+        model.add(MaxPool2D(pool_size=(2, 2)))
+        model.add(Dropout(0.3))
 
-            model.add(Flatten())
+        model.add(Flatten())
 
-            model.add(Dense(7, activation='softmax'))
-            model.summary()
-            tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
-            model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['acc'])
+        model.add(Dense(7, activation='softmax'))
+        model.summary()
+        tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['acc'])
 
-            model.fit(
-                x_train, y_train,
-                epochs=epochs,
-                batch_size=batch_size,
-                validation_data=(x_test, y_test),
-                verbose=2,
-                callbacks=[tensorboard])
-            score = model.evaluate(x_test, y_test)
-            print('Test accuracy:', score[1])
-            # Prediction on test data
-            y_pred = model.predict(x_test)
-            # Convert predictions classes to one hot vectors
-            y_pred_classes = np.argmax(y_pred, axis=1)
-            # Convert test data to one hot vectors
-            y_true = np.argmax(y_test, axis=1)
-            cm = confusion_matrix(y_true, y_pred_classes)
+        history = model.fit(
+            x_train, y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(x_test, y_test),
+            verbose=2,
+            callbacks=[tensorboard])
+        score = model.evaluate(x_test, y_test)
+        print('Test accuracy:', score[1])
+        y_pred = model.predict(x_test)
+        y_pred_classes = np.argmax(y_pred, axis=1)
+        y_true = np.argmax(y_test, axis=1)
+        cm = confusion_matrix(y_true, y_pred_classes)
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.set(font_scale=1.6)
+        sns.heatmap(cm, annot=True, linewidths=2, ax=ax)
+        plt.show()
+
+        incorr_fraction = 1 - np.diag(cm) / np.sum(cm, axis=1)
+        plt.bar(np.arange(7), incorr_fraction)
+        plt.xlabel('True Label')
+        plt.ylabel('Fraction of incorrect predictions')
+        plt.show()
+
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        epochs = range(1, len(loss) + 1)
+        plt.plot(epochs, loss, 'y', label='Training loss')
+        plt.plot(epochs, val_loss, 'r', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
+
+        acc = history.history['acc']
+        val_acc = history.history['val_acc']
+        plt.plot(epochs, acc, 'y', label='Training acc')
+        plt.plot(epochs, val_acc, 'r', label='Validation acc')
+        plt.title('Training and validation accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+        plt.show()
+        print(NAME)
+
+        test_loss, test_acc = model.evaluate(x_test, y_test)
+        print('Test loss:', test_loss)
+        print('Test accuracy:', test_acc)
+        y_pred_labels = np.argmax(y_pred, axis=1)
+        class_report = classification_report(np.argmax(y_test, axis=1), y_pred_labels, labels=[0, 1, 2, 3, 4, 5, 6])
+        print(class_report)
 
 
-            fig, ax = plt.subplots(figsize=(10, 10))
-            sns.set(font_scale=1.6)
-            sns.heatmap(cm, annot=True, linewidths=2, ax=ax)
-            plt.show()
-
-            # PLot fractional incorrect misclassifications
-            incorr_fraction = 1 - np.diag(cm) / np.sum(cm, axis=1)
-            plt.bar(np.arange(7), incorr_fraction)
-            plt.xlabel('True Label')
-            plt.ylabel('Fraction of incorrect predictions')
-            plt.show()
-            print(NAME)
-
-# # conv->pool->conv->pool
-# # conv nimmt mehrere pixel und fasst sie in einem zusammen
-# # Das Fenster shiftet zu den nächsten Pixel und macht weiter
-# # pooling (maxPooling) findet das max value von den conv fenstern
 # model = Sequential()
 # model.add(Conv2D(256, (3, 3), activation="relu", input_shape=(32, 32, 3)))
-# # model.add(BatchNormalization())
 # model.add(MaxPool2D(pool_size=(2, 2)))
 # model.add(Dropout(0.3))
 #
 # model.add(Conv2D(128, (3, 3), activation='relu'))
-# # model.add(BatchNormalization())
 # model.add(MaxPool2D(pool_size=(2, 2)))
 # model.add(Dropout(0.3))
 #
 # model.add(Conv2D(64, (3, 3), activation='relu'))
-# # model.add(BatchNormalization())
 # model.add(MaxPool2D(pool_size=(2, 2)))
 # model.add(Dropout(0.3))
 # model.add(Flatten())
 #
 # model.add(Dense(32))
 # model.add(Dense(7, activation='softmax'))
-#
-#
-# # Train
-# # You can also use generator to use augmentation during training.
-# # softmax sagt aus zu wv. prozent das model denkt, dass es die klasse ist
-#
 #
 #
 # score = model.evaluate(x_test, y_test)
@@ -125,14 +135,12 @@ for activation in activations:
 # plt.legend()
 # plt.show()
 #
-# # Prediction on test data
+
 # y_pred = model.predict(x_test)
-# # Convert predictions classes to one hot vectors
 # y_pred_classes = np.argmax(y_pred, axis=1)
-# # Convert test data to one hot vectors
+
 # y_true = np.argmax(y_test, axis=1)
-#
-# # Print confusion matrix
+
 # cm = confusion_matrix(y_true, y_pred_classes)
 #
 #
@@ -140,7 +148,6 @@ for activation in activations:
 # sns.set(font_scale=1.6)
 # sns.heatmap(cm, annot=True, linewidths=2, ax=ax)
 #
-# # PLot fractional incorrect misclassifications
 # incorr_fraction = 1 - np.diag(cm) / np.sum(cm, axis=1)
 # plt.bar(np.arange(7), incorr_fraction)
 # plt.xlabel('True Label')
